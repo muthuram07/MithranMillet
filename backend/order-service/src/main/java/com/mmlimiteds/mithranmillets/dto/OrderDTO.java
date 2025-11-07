422e4fa1c4c453d5468567823c226ff02eb11a1b
Muthuraman Saravanan <129589774+muthuram07@users.noreply.github.com>
2025-11-07 17:13:58 +0530

    Add files via upload

---
+++ backend/order-service/src/main/java/com/mmlimiteds/mithranmillets/dto/OrderDTO.java
@@ -0,0 +1,77 @@
+package com.mmlimiteds.mithranmillets.dto;
+
+import java.sql.Date;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+    private String username;
+    private Long id;
+    private Double totalAmount;
+    private String paymentMethod;
+    private String status;
+    private Date orderDate;
+    private AddressDTO address;
+    private String razorpayOrderId;
+    private String paymentStatus;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public AddressDTO getAddress() {
+		return address;
+	}
+	public void setAddress(AddressDTO address) {
+		this.address = address;
+	}
+	public String getRazorpayOrderId() {
+		return razorpayOrderId;
+	}
+	public void setRazorpayOrderId(String razorpayOrderId) {
+		this.razorpayOrderId = razorpayOrderId;
+	}
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+}
+
+
